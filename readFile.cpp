ff032f828db748ccc230a20e27e6025e159d2372
maethassaeyo <maethassaeyo@gmail.com>
2026-03-04 16:32:06 +0700

    Show bytes Real time

--- readFile.cpp
+++ readFile.cpp
@@ -1,21 +1,41 @@
-#include<iostream>
-#include<fstream>
-#include<string>
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
+#include <unistd.h>
 
 using namespace std;
 
-int main(){
+int main() {
     string line;
-    ifstream file("/proc/net/dev");
+    while(true){
 
-    if(file.is_open()){
-        while(getline(file,line)){
-            cout << line << endl;
-        }file.close();
-    }else{
-        cerr << "Unable to open file";
+        // เปิดไฟล์สำหรับอ่าน
+        ifstream file("/proc/net/dev");
+        // ตรวจสอบว่าเปิดไฟล์ได้หรือไม่
+        if (file.is_open()) {
+                // อ่านไฟล์ทีละบรรทัด
+            while (getline(file, line)) {
+            
+                if(line.find("wlp") != string::npos){
+                    stringstream ss(line);
+                    string iface;
+                    long long recv_bytes;
+
+                    ss >> iface;
+                    ss >> recv_bytes;
+                        cout << iface << " " << recv_bytes / 1024 << "KB" << endl;
+                }
+
+            }   
+            // ปิดไฟล์
+            file.close();
+            sleep(1);
+        } else {
+            cerr << "Unable to open file";
+        }
     }
 
     return 0;
-
 }
+
