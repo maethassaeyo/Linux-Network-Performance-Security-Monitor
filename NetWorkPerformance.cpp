2eb9b984e891a4560fd279d7e8493bee183a5bec
maethassaeyo <maethassaeyo@gmail.com>
2026-03-05 11:11:42 +0700

    add funsion uplode-downlode Speed

--- NetWorkPerformance.cpp
+++ NetWorkPerformance.cpp
@@ -6,9 +6,18 @@
 #include <iomanip>   // สำหรับจัดรูปแบบตาราง
 using namespace std;
 
+double speed_kb(int current_recv,int prev_recv){
+    double speed = 0;
+    
+    speed = (current_recv - prev_recv) / 1024.0;
+
+    return speed;
+}
+
 int main() {
     string line;
-    long long prev_recv = 0;
+    long long prev_recv_down = 0;
+    long long prev_recv_up = 0;
     bool first_run = true;
 
     cout << fixed << setprecision(2);
@@ -16,7 +25,8 @@ int main() {
     while(true){
 
         ifstream file("/proc/net/dev");
-        long long current_recv = 0;
+        long long current_recv_down = 0;
+        long long current_recv_up = 0;
         string name = "";
         // ตรวจสอบว่าเปิดไฟล์ได้หรือไม่
         if (file.is_open()) {
@@ -25,29 +35,38 @@ int main() {
             
                 if(line.find("wlp") != string::npos){
                     stringstream ss(line);
-                    string iface;
-                    long long recv_bytes;
-
+                    string iface,junk;
+                    long long recv_bytes,uplode_bytes;
+                    
                     ss >> iface;
                     ss >> recv_bytes;
-                     
+                    for(int i = 0;i < 7;i++){
+                        ss >> junk;
+                    } 
+                    ss >> uplode_bytes;
+
+                    cout << uplode_bytes << endl;
 
                     name = iface;
-                    current_recv = recv_bytes;
+                    current_recv_down = recv_bytes;
+                    current_recv_up = uplode_bytes;
                 }
 
             }  
             // ปิดไฟล์
             file.close();
-            double speed_kb = 0;
+            double down_speed_kb = 0;
+            double up_speed_kb = 0;
             
             if(!first_run){
         
-                speed_kb = (current_recv - prev_recv) / 1024.0;
+                down_speed_kb = speed_kb(current_recv_down,prev_recv_down);
+                up_speed_kb = speed_kb(current_recv_up,prev_recv_up);
 
             }
-            cout << speed_kb << endl;
-            prev_recv = current_recv;
+            cout << "Downlond Speed : " << down_speed_kb << "\nUplode Speed : " << up_speed_kb << endl;
+            prev_recv_down = current_recv_down;
+            prev_recv_up = current_recv_up;
             first_run = false;
 
             sleep(1);
