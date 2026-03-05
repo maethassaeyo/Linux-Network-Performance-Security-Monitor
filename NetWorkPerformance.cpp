2061f2a0a3b939e12d6c7ed4ca0d6fcc4e067483
maethassaeyo <maethassaeyo@gmail.com>
2026-03-05 10:42:45 +0700

    edit file name readFile.cpp to NetWorkPerformance.cpp

--- NetWorkPerformance.cpp
+++ NetWorkPerformance.cpp
@@ -3,15 +3,21 @@
 #include <string>
 #include <sstream>
 #include <unistd.h>
-
+#include <iomanip>   // สำหรับจัดรูปแบบตาราง
 using namespace std;
 
 int main() {
     string line;
+    long long prev_recv = 0;
+    bool first_run = true;
+
+    cout << fixed << setprecision(2);
+
     while(true){
 
-        // เปิดไฟล์สำหรับอ่าน
         ifstream file("/proc/net/dev");
+        long long current_recv = 0;
+        string name = "";
         // ตรวจสอบว่าเปิดไฟล์ได้หรือไม่
         if (file.is_open()) {
                 // อ่านไฟล์ทีละบรรทัด
@@ -24,12 +30,26 @@ int main() {
 
                     ss >> iface;
                     ss >> recv_bytes;
-                        cout << iface << " " << recv_bytes / 1024 << "KB" << endl;
+                     
+
+                    name = iface;
+                    current_recv = recv_bytes;
                 }
 
-            }   
+            }  
             // ปิดไฟล์
             file.close();
+            double speed_kb = 0;
+            
+            if(!first_run){
+        
+                speed_kb = (current_recv - prev_recv) / 1024.0;
+
+            }
+            cout << speed_kb << endl;
+            prev_recv = current_recv;
+            first_run = false;
+
             sleep(1);
         } else {
             cerr << "Unable to open file";
